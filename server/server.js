861f9d454df96e0ec524b85a1ed8c6f27ff00abb
Artem1998 <arty-hero@yandex.ru>
2017-12-21 22:58:47 -0500

    Add geolocation link sending

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express= require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -22,17 +22,16 @@ io.on('connection', (socket)=>{
   socket.on('createMessage', (message, callback)=>{
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from server');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
+  });
+
+  socket.on('createLocationMessage', (coords)=>{
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', ()=>{
     console.log('User disconnected');
-  })
-})
+  });
+});
 
 server.listen(port, ()=>{
   console.log(`Server is up on port ${port}`);
