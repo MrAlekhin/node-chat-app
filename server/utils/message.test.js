0dfe67f5029a03723dc1bf66b317d6312eac04a9
Artem1998 <arty-hero@yandex.ru>
2017-12-21 19:07:51 -0500

    Add generateMessage method test

---
+++ server/utils/message.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var {generateMessage} = require('./message')
+
+describe('generateMessage', () => {
+  it('should generate correct message oblect', ()=>{
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});
