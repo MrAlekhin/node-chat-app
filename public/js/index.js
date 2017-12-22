e7d145dfcf328c7e4cb09764d5ecf1d9f610e412
Artem1998 <arty-hero@yandex.ru>
2017-12-22 03:15:00 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,8 @@ socket.on('newMessage', function(message){
 
 socket.on('newLocationMessage', function(message){
   var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank" >My current location</a>')
+  var a = jQuery('<a target="_blank">My current location</a>')
+
   li.text(`${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
@@ -27,11 +28,13 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function(){
-
+    messageTextbox.val('');
   });
 });
 
@@ -41,12 +44,16 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
-    })
+    });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.');
   });
 });
