1addede22dff8afa188334432ae72f8bb9e0f5f1
Artem1998 <arty-hero@yandex.ru>
2017-12-22 23:38:23 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom(){
+  //selectors
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child')
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function(){
   console.log('Connected to server');
 });
@@ -18,6 +35,7 @@ socket.on('newMessage', function(message){
   })
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -29,6 +47,7 @@ socket.on('newLocationMessage', function(message){
     url: message.url
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', function(e){
