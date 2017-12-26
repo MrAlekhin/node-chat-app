eab66b4d92882cda1e6d9da0e0f27a60fa9d2237
Artem1998 <arty-hero@yandex.ru>
2017-12-25 22:49:25 -0500

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -35,7 +35,7 @@ socket.on('disconnect', function(){
 });
 
 socket.on('updateUserList', function(users){
-  var ol = jQuery('<ol></ol>');
+  var ol = jQuery('<ul></ul>');
   users.forEach(function(user){
     ol.append(jQuery('<li></li>').text(user));
   });
@@ -74,7 +74,6 @@ jQuery('#message-form').on('submit', function(e){
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function(){
     messageTextbox.val('');
