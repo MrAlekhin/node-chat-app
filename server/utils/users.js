79e919cec34ca40c2177cde331073f768abeb280
Artem1998 <arty-hero@yandex.ru>
2017-12-25 19:26:44 -0500

    Add Users class methods

--- server/utils/users.js
+++ server/utils/users.js
@@ -19,6 +19,22 @@ class Users{
     this.users.push(user);
     return user;
   }
+  removeUser (id){
+    var user = this.getUser(id);
+    if(user){
+      this.users = this.users.filter((user)=>user.id !== id);
+    }
+    return user;
+  }
+  getUser(id){
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList(room){
+    var users = this.users.filter((user)=>user.room === room);
+    var namesArray = users.map((user)=>user.name);
+
+    return namesArray;
+  }
 }
 
 
